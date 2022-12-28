9d9c271a7fc84efb8d97a23c2e2cdf0a18e85b3e
RobPruzan <rapruzan@buffalo.edu>
2022-12-28 16:28:20 -0500

    Setting up tailwind css

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
 import MainPage from './components/MainPage';
