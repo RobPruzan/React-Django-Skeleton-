f16ce69c8abd9ad7f1e6baf4bc4aa7d6957047e6
RobPruzan <rapruzan@buffalo.edu>
2022-12-29 01:23:54 -0500

    Adding provider + devtools for react-query

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,14 +3,20 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './redux/store';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
+const queryClient = new QueryClient();
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>
 );
